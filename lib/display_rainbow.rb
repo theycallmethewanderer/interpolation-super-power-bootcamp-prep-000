31edbb9d544ae6bb823f1bf4119bbb6bbafde4fe
Rahm Lapid <rahm.lapid@gmail.com>
2018-12-29 22:20:52 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,5 +2,5 @@
 def display_rainbow(colors)
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 puts "R: #{display_rainbow[0]}, O: #{display_rainbow[1]}, Y: #{display_rainbow[2]}, G: #{display_rainbow[3]},
-      B: #{display_rainbow[4]}, I: #{display_rainbow[5]}, V: #{display_rainbow[6]}, "
+      B: #{display_rainbow[4]}, I: #{display_rainbow[5]}, V: #{display_rainbow[6]}"
 end
